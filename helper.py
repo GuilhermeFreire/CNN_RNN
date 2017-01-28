7c22df9636aef586ad4db4427a273f68fc85ff04
Guilherme <guilhermebfreire@gmail.com>
2017-01-28 07:09:33 -0300

    Separated the returns un image and descriptions iterators instead of tuples. WARNING: weird bug with load_n_images

--- helper.py
+++ helper.py
@@ -40,20 +40,44 @@ class Helper:
 		#Carrega uma imagem em um numpy array
 		return np.asarray(Image.open(self.complimentary_path + path).getdata()).reshape(self.img_dims)
 
+	def load_n_images(self, images):
+		if(type(images) == int):
+			print("ifou")
+			examples = range(images)
+		else:
+			print("elsou")
+			examples = images
+		for i in examples:
+			print(self.load_single_image(self.img_paths[i]))
+			print("-------------------")
+			yield self.load_single_image(self.img_paths[i])
+
+	def load_n_descriptions(self, descriptions):
+		if(type(descriptions) == int):
+			examples = range(descriptions)
+		else:
+			examples = descriptions
+		for i in examples:
+			yield self.load_single_description(i)
+
+
 	def load_n_examples(self, n):
 		#Carrega n exemplos sequenciais do dataset
-		for i in range(n):
-			yield (self.load_single_image(self.img_paths[i]), self.load_single_description(i))
+		# for i in range(n):
+			# yield (self.load_single_image(self.img_paths[i]), self.load_single_description(i))
+		return (self.load_n_images(n), self.load_n_descriptions(n))
 
 	def load_n_random_examples(self, n):
 		#Carrega n exemplos aleatórios do dataset
-		for i in range(n):
-			r = np.random.random_integers(0,self.num_examples)
-			yield (self.load_single_image(self.img_paths[r]), self.load_single_description(r))
+		# for i in range(n):
+		r = np.random.random_integers(0,self.num_examples, n)
+		return (self.load_n_images(r), self.load_n_descriptions(r))
+			# yield (self.load_single_image(self.img_paths[r]), self.load_single_description(r))
 
 	def load_all_examples(self):
-		for i in range(self.num_examples):
-			yield (self.load_single_image(self.img_paths[i]), self.load_single_description(i))
+		return self.load_all_examples(self.num_examples)
+		# for i in range(self.num_examples):
+		# 	yield (self.load_single_image(self.img_paths[i]), self.load_single_description(i))
 
 	def load_dataset(self, meta_data_path="metadata.txt"):
 		#Lê os metadados do arquivo
@@ -84,6 +108,27 @@ if __name__ == "__main__":
 	h = Helper()
 	h.load_dataset()
 
-	for X, Y in h.load_n_random_examples(5):
-		print("X:", np.asarray(X).shape, " Y:", np.asarray(Y).shape)
-		print(Y, end="\n\n")
+	# for X, Y in h.load_n_random_examples(5):
+	# 	print("X:", np.asarray(X).shape, " Y:", np.asarray(Y).shape)
+	# 	print(Y, end="\n\n")
+	X, Y = h.load_n_random_examples(5)
+	# print(X)
+	# print(Y)
+	flag = 0
+	for i in X:
+		# if flag == 0:
+		# 	flag = 1
+		# 	for j in i:
+		# 		print(j)
+		print(X)
+
+	for i in Y:
+		print(i)
+
+	ran = np.random.random_integers(0,h.num_examples, 3)
+	test = h.load_n_images(ran)
+	for t in test:
+		print(t)
+	print(ran)
+
+	print("Total characters in dataset:", np.sum(list(map(lambda s: len(s), h.descriptions))))
